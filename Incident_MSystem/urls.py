bc1bff4fbd7c7f918e84d02ce02f6fbebf4a8136
x24250511 <tejas@Tejass-MacBook-Air.local>
2025-12-03 14:42:00 +0000

    media uplaod

--- Incident_MSystem/urls.py
+++ Incident_MSystem/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
